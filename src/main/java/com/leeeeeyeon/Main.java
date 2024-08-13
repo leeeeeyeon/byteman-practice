b1f4d8c0c818c00732a2829c87e86fe4a5569b35
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-08-14 01:01:34 +0900

    increase method latency example

--- src/main/java/com/leeeeeyeon/Main.java
+++ src/main/java/com/leeeeeyeon/Main.java
@@ -2,9 +2,22 @@ package com.leeeeeyeon;
 
 public class Main {
 
+    public static void printHello() {
+        System.out.println("Hello, World!");
+    }
+
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
         for (String arg : args) {
             System.out.println(arg);
         }
+        printHello();
+
+        long end = System.currentTimeMillis();
+
+        long executeTimeSec = (end - start) / 1000;
+
+        System.out.println("Execute Time: " + executeTimeSec + " sec");
     }
 }
