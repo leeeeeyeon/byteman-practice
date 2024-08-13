a43a0c13494a07c7c0a2806d801b8cc5f8a1dc46
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-08-14 00:07:52 +0900

    initial commit

---
+++ src/main/java/com/leeeeeyeon/Main.java
@@ -0,0 +1,10 @@
+package com.leeeeeyeon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
