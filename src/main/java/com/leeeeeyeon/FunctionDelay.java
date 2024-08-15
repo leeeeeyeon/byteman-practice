27729d5ae106c8a3cec3036606505206c9e8d17b
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-08-15 23:41:39 +0900

    function delay example with multiple functions

--- src/main/java/com/leeeeeyeon/FunctionDelay.java
+++ src/main/java/com/leeeeeyeon/FunctionDelay.java
@@ -6,13 +6,25 @@ public class FunctionDelay {
         System.out.println("Hello, World!");
     }
 
+    public static void printBye() {
+        System.out.println("Bye, World!");
+    }
+
     public static void main(String[] args) {
+        // printHello();
         long start = System.currentTimeMillis();
         printHello();
         long end = System.currentTimeMillis();
 
         long executeTimeSec = (end - start) / 1000;
+        System.out.println("printHello: " + executeTimeSec + " sec");
 
-        System.out.println("Execute Time: " + executeTimeSec + " sec");
+        // printBye
+        start = System.currentTimeMillis();
+        printBye();
+        end = System.currentTimeMillis();
+
+        executeTimeSec = (end - start) / 1000;
+        System.out.println("printHello: " + executeTimeSec + " sec");
     }
 }
